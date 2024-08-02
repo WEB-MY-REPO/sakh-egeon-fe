923f0d1495454e69d1a7704bac53b369db742435
unknown <predator_x_x_x@mail.ru>
2024-08-03 09:22:18 +1100

    ssh key

--- uploadscript.cmd
+++ uploadscript.cmd
@@ -1,4 +1,4 @@
-open sftp://%1%:%2%@151.248.122.7/ -hostkey="ssh-rsa 3072 wTIRAYg366AK+7qm1su/eueZYX2Gc5f3r4VG1g0Du28"
+open sftp://%1%:%2%@151.248.122.7/ -hostkey="ssh-rsa 3072 DuouOomp83+jeyqAoGRL7wy1y/aaWpkOWzgMbc1bazc"
 cd /home/%1%/ooo-egeon-be/public
 call mkdir ../jsons 2>/dev/null || :
 call cp -f *.json ../jsons/ 2>/dev/null || :
