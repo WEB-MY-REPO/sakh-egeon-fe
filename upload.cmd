1a395d37db5db1584a63184cf63188701deb068d
unknown <predator_x_x_x@mail.ru>
2024-08-03 09:21:30 +1100

    Upload pwd

--- upload.cmd
+++ upload.cmd
@@ -4,4 +4,4 @@ del /f public.7z 2>&1>NUL
 cd dist
 "C:\Program Files\7-Zip\7z" a -mx9 ../public
 cd ..
-"C:\Program Files (x86)\WinSCP\WinSCP.com" /ini=nul /script=uploadscript.cmd /parameter sakh-egeon xs12!#58$_WZy
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /ini=nul /script=uploadscript.cmd /parameter sakh-egeon dsfWXZLJ_1SZDDsssQ
